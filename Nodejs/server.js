6e4c19ff49bd15eb2adbe5eeb58c80ba67ab8562
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-12-28 20:21:18 -0300

    Instalando o nodemon no projeto e mudando o titulo do server.js

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -7,8 +7,9 @@ http
     var url = req.url;
     if (url === "/stats") {//write a response
       res.end(JSON.stringify(stats, null, 2)); //end the response
+      
     } else {
-      res.write("<h1>Working</h1>"); //write a response
+      res.write("<h1>Sou um servidor rodando</h1>"); //write a response
       res.end();
     }
   })
